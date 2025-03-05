b536a6ded9235b3869c1a706b7a65a5feac25cee
Brian <brian.sila1@student.moringaschool.com>
2025-03-05 08:59:19 +0300

    arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Define num1 and num2 for multiply test
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// Define random for random test
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Define num3 and num4 for mod test
+const num3 = 14;
+const num4 = 10;
+const mod = num3 % num4;
+
+// Define max for max test
+const max = Math.max(1, 2, 3, 20);
